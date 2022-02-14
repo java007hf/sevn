5fa9de92c1b575f648f375aa8a26d1946dee1973
java007hf <102wangyingli@163.com>
2022-02-15 07:58:32 +0800

    增加部分商品bean

--- src/main/java/com/xsoft/sevn/dtk/Commodit.java
+++ src/main/java/com/xsoft/sevn/dtk/Commodit.java
@@ -1,4 +1,16 @@
 package com.xsoft.sevn.dtk;
 
 public class Commodit {
+    String brand_name; //商品名称 王老吉
+    String brand_id; //商品id 3378051
+    String add_time; //上架时间 2022-02-14 21:31:02
+    String category_name; //商品类型 凉茶
+    String commission_rate; //佣金百分比 4.25
+    String coupon_amount; //优惠券 5
+    String coupon_end_time; //优惠券结束时间 2022-02-15 23:59:59
+    String coupon_end_time_stamp; //优惠券结束时间 1644940799
+    String coupon_num; //优惠券总数 1000
+    String coupon_over; //优惠券使用 0
+    String d_title; //商品title 降火必备！王老吉凉茶植物饮料310ml*24罐
+
 }
